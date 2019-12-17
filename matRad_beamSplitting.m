01ffe174f22cc89b4c8de29c9f08a4a9b0d474f8
Paul Anton Meder <p.meder@protonmail.com>
2019-12-17 17:50:47 +0100

    added 2 dim beamsplitting

--- matRad_beamSplitting.m
+++ matRad_beamSplitting.m
@@ -1,91 +1,50 @@
-clear
-
-%% one dimension
+clear all, close all
 weight = @(mu, sigma, i, deltaR) integral(@(x) normpdf(x, mu, sigma), mu + (i - 0.5) * deltaR, mu + (i + 0.5) * deltaR);
 
 % plot original gauss
 sigma = 10;
 mu = 0;
-x = -40:0.1:40;
-plot(x, normpdf(x,mu,sigma));
-hold on
 
 % plot splitted gaussians
 N = 21;
-sigmaSub = 1;
+sigmaSub = 2;
 sigmaDach = sqrt(sigma^2 - sigmaSub^2);
 deltaR = 7 * sigmaDach / N;
-sum = zeros(size(x,2),1);
+
+gri = zeros(N,1);
+xVal = zeros(N,1);
+count = 1;
 for i = -(N - 1) / 2 : (N - 1) / 2
-    beamletGauss = @(x) weight(mu, sigmaDach, i, deltaR) * normpdf(x, mu + i * deltaR, sigmaSub);
-    plot(x, beamletGauss(x));
-    sum = sum + beamletGauss(x)';
+    gri(count)  = weight(mu, sigmaDach, i, deltaR);
+    xVal(count) = mu + i * deltaR;
+    count = count + 1;
+end
+
+yVal = xVal;
+weightGrid = gri * gri';
+
+M = 20;
+length = 80;
+for i = 1:M
+    for j = 1:M
+       coord(i,j,:) = [i*length/M - length/2, j*length/M - length/2];
+    end
+end
+
+gauss2dCoord = @(coord, mu, sigma) 1 / (2 * pi * sigma^2) .* exp(-(coord(:,:,1) - mu(1)).^2 / (2 * sigma^2)) .* exp(-(coord(:,:,2) - mu(2)).^2 / (2 * sigma^2));
+        
+sum = zeros(M,M);
+hold on
+for i = 1:N
+    for j = 1:N
+        erg = gauss2dCoord(coord, [xVal(i), yVal(j)], sigmaSub) * weightGrid(i,j);
+        surf(erg);
+        sum = sum + erg;
+    end
 end
 hold off
 
+figure 
+surf(sum - gauss2dCoord(coord, [0, 0], sigma));
 figure
-plot(x, sum)
-hold on
-plot(x, normpdf(x,mu,sigma));
-plot(x, abs(sum - normpdf(x,mu,sigma)'));
-hold off
-%% two dimensions
-
-gauss2d = @(x, y, mu, sigma) 1 / (2 * pi * sigma^2) * exp(-(sqrt(x^2 + y^2) - mu)^2 / (2 * sigma^2));
-
-for m = 1:M
-   tmpX = linspace(-40,40,1000);
-   tmpY = 
-        
-
-surf(X, Y, gauss2d(X, Y, 0, 10));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% radialGauss = @(r, mu, sigma) 1 / sqrt(2 * pi * sigma^2) .* exp(-(r - mu).^2 /(2 * sigma^2));
-% 
-% nTheta = 8;
-% nR = 3;
-% fn = 1;
-% sigma = 5;
-% rMax = 3 * sigma;
-% dR  = rMax / nR;
-% dTheta = 360 / nTheta;
-% 
-% 
-% theta = 0;
-% i=1;
-% r = 0;
-% sigmaSub = 2.1;
-% F = fn * dTheta / (2 * pi) *(exp(-(0)^2/(2*sigmaSub^2)) - exp(-(dR/2)^2/(2*sigmaSub^2)));
-% erg(i,:) = [r, theta, F];
-% i = 2;
-% for iTheta = 1:nTheta
-%     r = dR;
-%     for iR = 1:nR
-%         F = fn * dTheta / (2 * pi) *(exp(-(r - dR/2)^2/(2*sigmaSub^2)) - exp(-(r + dR/2)^2/(2*sigmaSub^2)));
-%         erg(i,:) = [r, theta, F];
-%         r = r + dR;
-%         i = i + 1;
-%     end
-%     theta = theta + dTheta;
-% end
-% format short; 
-% erg
+surf(sum);
