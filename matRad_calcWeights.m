9a74b9a302d24c659ae49db1997639a5de8ca5e5
Paul Anton Meder <p.meder@protonmail.com>
2019-12-17 18:01:20 +0100

    create weight calculating function

---
+++ matRad_calcWeights.m
@@ -0,0 +1,32 @@
+function [weights, xPos, yPos] = matRad_calcWeights(sigma, mu, N, sigmaSub)
+% Russo splitting
+
+sigmaDach = sqrt(sigma^2 - sigmaSub^2);
+deltaR = 7 * sigmaDach / N;
+
+weight = @(mu, sigma, i, deltaR) integral(@(x) normpdf(x, mu, sigma), mu + (i - 0.5) * deltaR, mu + (i + 0.5) * deltaR);
+
+gri = zeros(N,1);
+coord = zeros(N,1);
+count = 1;
+for i = -(N - 1) / 2 : (N - 1) / 2
+    gri(count)  = weight(mu, sigmaDach, i, deltaR);
+    coord(count) = mu + i * deltaR;
+    count = count + 1;
+end
+
+weightGrid = gri * gri';
+
+weights = zeros(N^2,1);
+xPos    = zeros(N^2,1);
+yPos    = zeros(N^2,1);
+
+count = 1;
+for i = 1:N
+    for j = 1:N
+        weights(count) = weightGrid(i,j);
+        xPos(count) = coord(i);
+        yPos(count) = coord(j);
+        count = count + 1;
+    end
+end
